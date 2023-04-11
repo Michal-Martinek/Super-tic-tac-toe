8ffef3e22ad99cc9e79b129dbe3074b55f18a3ea
Michal Martínek <michal7952@seznam.cz>
2023-04-11 21:50:07 +0200

    Winning small boards added

--- Super_tic_tac_toe.py
+++ Super_tic_tac_toe.py
@@ -6,7 +6,10 @@ BOARD_Y = 50
 CELL_SIZE = WIDTH // 9
 
 CELL_INNER_SIZE = int(CELL_SIZE * 3/4)
+CELL_PADDING = (CELL_SIZE - CELL_INNER_SIZE) // 2
 MARK_THICKNESS = 5
+BIG_MARK_THICKESS_ADD = 8
+
 
 HEADER_LINE_THICKNESS = 5
 BIG_LINE_THICKNESS = 3
@@ -14,6 +17,7 @@ SMALL_LINE_THICKNESS = 1
 
 LINE_COLOR = (255, 255, 255)
 BACKGROUND_COLOR = (0, 0, 0)
+SHADOW_MARK_COLOR = (128, 128, 128)
 
 FPS = 60
 
@@ -31,19 +35,38 @@ class SmallBoard:
 		self.board: list[list[int]] = [[0] * 3 for i in range(3)] # 0 - unplayed, 1 - first, 2 - second
 		self.row = row
 		self.col = col
+		self.won = 0 # 0, 1, 2
 	
+	def makeMove(self, X, Y, move) -> bool:
+		assert not self.won, 'cannot make moves to won sub-boards'
+		self.board[Y][X] = move
+		self.won = self.isWon()
+		return self.won
+	def isWon(self) -> int:
+		for i in range(3):
+			row = all([tile == self.board[i][0] for tile in self.board[i]]) and self.board[i][0]
+			col = all([self.board[j][i] == self.board[0][i] for j in range(3)]) and self.board[0][i]
+			if row or col: return row or col
+		dia1 = self.board[0][0] == self.board[1][1] == self.board[2][2]
+		dia2 = self.board[0][2] == self.board[1][1] == self.board[2][0]
+		return (dia1 and self.board[0][0]) or (dia2 and self.board[0][2])
 	def getFree(self, Xoff=0, Yoff=0) -> set[tuple[int, int]]:
+		if self.won: return set()
 		return set([(x + Xoff * 3, y + Yoff * 3) for x in range(3) for y in range(3) if self.board[y][x] == 0])
 
 	@ staticmethod
-	def _drawFirst(display, pos): # cross
-		points = [(pos[0] + Xoff, pos[1] + Yoff) for Yoff in (CELL_SIZE - CELL_INNER_SIZE, CELL_INNER_SIZE) for Xoff in (CELL_SIZE - CELL_INNER_SIZE, CELL_INNER_SIZE)]
-		pygame.draw.line(display, (255, 0, 0), points[0], points[-1], MARK_THICKNESS)
-		pygame.draw.line(display, (255, 0, 0), points[1], points[2], MARK_THICKNESS)
+	def _drawFirst(display, pos, shadow=False, big=False): # cross
+		endOff = CELL_SIZE - CELL_PADDING if not big else 3 * CELL_SIZE - CELL_PADDING
+		points = [(pos[0] + Xoff, pos[1] + Yoff) for Yoff in (CELL_PADDING, endOff) for Xoff in (CELL_PADDING, endOff)]
+		color = (255, 0, 0) if not shadow else SHADOW_MARK_COLOR
+		pygame.draw.line(display, color, points[0], points[-1], MARK_THICKNESS + big * BIG_MARK_THICKESS_ADD)
+		pygame.draw.line(display, color, points[1], points[2], MARK_THICKNESS + big * BIG_MARK_THICKESS_ADD)
 	@ staticmethod
-	def _drawSecond(display, pos): # circle
+	def _drawSecond(display, pos, shadow=False, big=False): # circle
 		pos = pos[0] + CELL_SIZE // 2, pos[1] + CELL_SIZE // 2
-		pygame.draw.circle(display, (0, 0, 255), pos, CELL_INNER_SIZE // 2, MARK_THICKNESS)
+		if big: pos = pos[0] + CELL_SIZE, pos[1] + CELL_SIZE
+		radius = CELL_INNER_SIZE // 2 if not big else CELL_SIZE * 3 // 2 - CELL_PADDING
+		pygame.draw.circle(display, (0, 0, 255) if not shadow else SHADOW_MARK_COLOR, pos, radius, MARK_THICKNESS)
 
 	def draw(self, display, opened: set[tuple[int, int]]):
 		pos = self.col * CELL_SIZE * 3, self.row * CELL_SIZE * 3 + BOARD_Y
@@ -51,12 +74,13 @@ class SmallBoard:
 			for x, cell in enumerate(cells):
 				cellPos = pos[0] + x * CELL_SIZE, pos[1] + y * CELL_SIZE
 				if cell == 1:
-					self._drawFirst(display, cellPos)
+					self._drawFirst(display, cellPos, self.won)
 				elif cell == 2:
-					self._drawSecond(display, cellPos)
+					self._drawSecond(display, cellPos, self.won)
 				elif (x + self.col * 3, y + self.row * 3) in opened:
-					cellPadd = (CELL_SIZE - CELL_INNER_SIZE) // 2
-					pygame.draw.rect(display, (5, 91, 115), (pos[0] + CELL_SIZE * x + cellPadd, pos[1] + CELL_SIZE * y + cellPadd, CELL_INNER_SIZE, CELL_INNER_SIZE))
+					pygame.draw.rect(display, (5, 91, 115), (pos[0] + CELL_SIZE * x + CELL_PADDING, pos[1] + CELL_SIZE * y + CELL_PADDING, CELL_INNER_SIZE, CELL_INNER_SIZE))
+		if self.won:
+			(self._drawFirst, self._drawSecond)[self.won-1](display, (self.col * CELL_SIZE * 3, self.row * CELL_SIZE * 3 + BOARD_Y), big=True)
 
 class Board:
 	def __init__(self):
@@ -77,8 +101,11 @@ class Board:
 
 	def makeMove(self, pos: tuple[int, int], move) -> bool:
 		if free := (pos in self.opened):
-			self.board[pos[1] // 3][pos[0] // 3].board[pos[1] % 3][pos[0] % 3] = move
+			won = self.board[pos[1] // 3][pos[0] // 3].makeMove(pos[0] % 3, pos[1] % 3, move)
+			if won:
+				print('board won')
 			self.openSubBoard = pos[0] % 3, pos[1] % 3
+			if self.board[pos[1] % 3][pos[0] % 3].won: self.openSubBoard = (-1, -1)
 			self.opened = self.getOpened()
 		return free
 
